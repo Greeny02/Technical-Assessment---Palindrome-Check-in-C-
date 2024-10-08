3b8eda239810a1e99d0017c5acc4d7d165843ba6
Nabeel Crichton <nabeel.crichton@redpandasoftware.co.za>
2024-10-08 21:46:27 +0200

    Palindrome Logic fixed

---
+++ Technical Assessment - Palindrome Check/Technical Assessment - Palindrome Check/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Technical_Assessment___Palindrome_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Hello, please enter a word: ");
+                string word = Console.ReadLine();
+
+                List<char> wordList = new List<char>();
+
+                foreach (char item in word)
+                {
+                    wordList.Add(item);
+                }
+
+                string reverseWord = "";
+                for (int i = word.Length; i > 0; i--)
+                {
+                    reverseWord += wordList[i - 1];
+                }
+
+                Console.WriteLine(reverseWord);
+
+                if (reverseWord == word)
+                {
+                    Console.WriteLine($"{word} is a Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine($"{word} is not a Palindrome");
+                }
+            }
+        }
+    }
+}
